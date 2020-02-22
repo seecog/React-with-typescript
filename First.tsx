3c9d34b670943a7f96c7f752a7ab810039ff6b36
pankaj.7613@gmail.com <pankaj.7613@gmail.com>
2020-02-22 17:47:08 +0530

    done

---
+++ First.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default class First extends React.Component{
+
+    render(){
+        return (
+            <div>
+                <h2>Inside First component</h2>
+            </div>
+        )
+    }
+}
---
+++ app.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import First from './First';
+ReactDOM.render(
+<div>
+    <h1>Welcome to page</h1>
+    <First/>
+</div>,document.getElementById('root')
+)
